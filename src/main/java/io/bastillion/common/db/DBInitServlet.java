b0c19a7431a8156d5ede8cce9760a1e35901d2b9
skavanagh <sean.p.kavanagh6@gmail.com>
2019-05-18 08:08:57 -0400

    Added license file check

--- src/main/java/io/bastillion/common/db/DBInitServlet.java
+++ src/main/java/io/bastillion/common/db/DBInitServlet.java
@@ -54,8 +54,8 @@ import java.util.Scanner;
  * Initial startup task.  Creates an H2 DB.
  */
 @WebServlet(name = "DBInitServlet",
-        urlPatterns = {"/config"},
-        loadOnStartup = 1)
+		urlPatterns = {"/config"},
+		loadOnStartup = 1)
 public class DBInitServlet extends javax.servlet.http.HttpServlet {
 
     private static Logger log = LoggerFactory.getLogger(DBInitServlet.class);
@@ -67,52 +67,51 @@ public class DBInitServlet extends javax.servlet.http.HttpServlet {
      */
     public void init(ServletConfig config) throws ServletException {
 
-        super.init(config);
+		super.init(config);
 
+		Connection connection = null;
+		Statement statement = null;
+		//check if reset ssh application key is set
+		boolean resetSSHKey = "true".equals(AppConfig.getProperty("resetApplicationSSHKey"));
 
-        Connection connection = null;
-        Statement statement = null;
-        //check if reset ssh application key is set
-        boolean resetSSHKey = "true".equals(AppConfig.getProperty("resetApplicationSSHKey"));
+		//if DB password is empty generate a random
+		if(StringUtils.isEmpty(AppConfig.getProperty("dbPassword"))) {
+			String dbPassword = null;
+			String dbPasswordConfirm = null;
+			if(!"true".equals(System.getProperty("GEN_DB_PASS"))) {
+				//prompt for password and confirmation
+				while (dbPassword == null || !dbPassword.equals(dbPasswordConfirm)) {
+					if (System.console() == null) {
+						Scanner in = new Scanner(System.in);
+						System.out.println("Please enter database password: ");
+						dbPassword = in.nextLine();
+						System.out.println("Please confirm database password: ");
+						dbPasswordConfirm = in.nextLine();
+					} else {
+						dbPassword = new String(System.console().readPassword("Please enter database password: "));
+						dbPasswordConfirm = new String(System.console().readPassword("Please confirm database password: "));
+					}
+					if (!dbPassword.equals(dbPasswordConfirm)) {
+						System.out.println("Passwords do not match");
+					}
+				}
+			}
+			//set password
+			if(StringUtils.isNotEmpty(dbPassword)) {
+				AppConfig.encryptProperty("dbPassword", dbPassword);
+			//if password not set generate a random
+			} else {
+				System.out.println("Generating random database password");
+				AppConfig.encryptProperty("dbPassword", RandomStringUtils.random(32, true, true));
+			}
+		//else encrypt password if plain-text
+		} else if (!AppConfig.isPropertyEncrypted("dbPassword")) {
+			AppConfig.encryptProperty("dbPassword", AppConfig.getProperty("dbPassword"));
+		}
 
-        //if DB password is empty generate a random
-        if(StringUtils.isEmpty(AppConfig.getProperty("dbPassword"))) {
-            String dbPassword = null;
-            String dbPasswordConfirm = null;
-            if(!"true".equals(System.getProperty("GEN_DB_PASS"))) {
-                //prompt for password and confirmation
-                while (dbPassword == null || !dbPassword.equals(dbPasswordConfirm)) {
-                    if (System.console() == null) {
-                        Scanner in = new Scanner(System.in);
-                        System.out.println("Please enter database password: ");
-                        dbPassword = in.nextLine();
-                        System.out.println("Please confirm database password: ");
-                        dbPasswordConfirm = in.nextLine();
-                    } else {
-                        dbPassword = new String(System.console().readPassword("Please enter database password: "));
-                        dbPasswordConfirm = new String(System.console().readPassword("Please confirm database password: "));
-                    }
-                    if (!dbPassword.equals(dbPasswordConfirm)) {
-                        System.out.println("Passwords do not match");
-                    }
-                }
-            }
-            //set password
-            if(StringUtils.isNotEmpty(dbPassword)) {
-                AppConfig.encryptProperty("dbPassword", dbPassword);
-                //if password not set generate a random
-            } else {
-                System.out.println("Generating random database password");
-                AppConfig.encryptProperty("dbPassword", RandomStringUtils.random(32, true, true));
-            }
-            //else encrypt password if plain-text
-        } else if (!AppConfig.isPropertyEncrypted("dbPassword")) {
-            AppConfig.encryptProperty("dbPassword", AppConfig.getProperty("dbPassword"));
-        }
-
-        try {
-            connection = DBUtils.getConn();
-            statement = connection.createStatement();
+		try {
+			connection = DBUtils.getConn();
+			statement = connection.createStatement();
 
             ResultSet rs = statement.executeQuery("select * from information_schema.tables where upper(table_name) = 'USERS' and table_schema='PUBLIC'");
             if (rs == null || !rs.next()) {
@@ -133,17 +132,28 @@ public class DBInitServlet extends javax.servlet.http.HttpServlet {
                 statement.executeUpdate("create table if not exists terminal_log (session_id BIGINT, instance_id INTEGER, output varchar not null, log_tm timestamp default CURRENT_TIMESTAMP, display_nm varchar not null, user varchar not null, host varchar not null, port INTEGER not null, foreign key (session_id) references session_log(id) on delete cascade)");
                 statement.executeUpdate("create table if not exists license (id INTEGER PRIMARY KEY AUTO_INCREMENT, license_tx varchar not null)");
 
-                //if exists readfile to set default password
-                String salt = EncryptionUtil.generateSalt();
-                String defaultPassword = EncryptionUtil.hash("changeme" + salt);
-                File file = new File("/opt/bastillion-ec2/instance_id");
-                if (file.exists()) {
-                    String str = FileUtils.readFileToString(file, "UTF-8");
-                    if(StringUtils.isNotEmpty(str)) {
-                        defaultPassword = EncryptionUtil.hash(str.trim() + salt);
-                    }
-                    LicenseDB.saveLicense(LicenseUtil.generateForEC2());
-                }
+				//if exists readfile to set default password
+				String salt = EncryptionUtil.generateSalt();
+				String defaultPassword = EncryptionUtil.hash("changeme" + salt);
+
+				//set license if running in EC2
+				File file = new File("/opt/bastillion-ec2/instance_id");
+				if (file.exists()) {
+					String str = FileUtils.readFileToString(file, "UTF-8");
+					if(StringUtils.isNotEmpty(str)) {
+						defaultPassword = EncryptionUtil.hash(str.trim() + salt);
+					}
+					LicenseDB.saveLicense(LicenseUtil.generateForEC2());
+				}
+
+				//license key text file
+				file = new File(AppConfig.class.getClassLoader().getResource(".").getPath() + "../../../../LICENSE_KEY.txt");
+				if (file.exists()) {
+					String str = FileUtils.readFileToString(file, "UTF-8");
+					if(StringUtils.isNotEmpty(str)) {
+						LicenseDB.saveLicense(str.trim());
+					}
+				}
 
                 //insert default admin user
                 statement.executeUpdate("insert into users (username, password, user_type, salt) values('admin', '" + defaultPassword + "','"+ Auth.MANAGER+"','"+ salt+"')");
@@ -152,55 +162,55 @@ public class DBInitServlet extends javax.servlet.http.HttpServlet {
 
             DBUtils.closeRs(rs);
 
-            //if reset ssh application key then generate new key
-            if (resetSSHKey) {
+			//if reset ssh application key then generate new key
+			if (resetSSHKey) {
 
-                //delete old key entry
-                PreparedStatement pStmt = connection.prepareStatement("delete from application_key");
-                pStmt.execute();
-                DBUtils.closeStmt(pStmt);
+				//delete old key entry
+				PreparedStatement pStmt = connection.prepareStatement("delete from application_key");
+				pStmt.execute();
+				DBUtils.closeStmt(pStmt);
 
                 //generate new key and insert passphrase
                 System.out.println("Setting Bastillion for EC2 SSH public/private key pair");
 
-                //generate application pub/pvt key and get values
-                String passphrase = SSHUtil.keyGen();
-                String publicKey = SSHUtil.getPublicKey();
-                String privateKey = SSHUtil.getPrivateKey();
+				//generate application pub/pvt key and get values
+				String passphrase = SSHUtil.keyGen();
+				String publicKey = SSHUtil.getPublicKey();
+				String privateKey = SSHUtil.getPrivateKey();
 
-                //insert new keys
-                pStmt = connection.prepareStatement("insert into application_key (public_key, private_key, passphrase) values(?,?,?)");
-                pStmt.setString(1, publicKey);
-                pStmt.setString(2, EncryptionUtil.encrypt(privateKey));
-                pStmt.setString(3, EncryptionUtil.encrypt(passphrase));
-                pStmt.execute();
-                DBUtils.closeStmt(pStmt);
+				//insert new keys
+				pStmt = connection.prepareStatement("insert into application_key (public_key, private_key, passphrase) values(?,?,?)");
+				pStmt.setString(1, publicKey);
+				pStmt.setString(2, EncryptionUtil.encrypt(privateKey));
+				pStmt.setString(3, EncryptionUtil.encrypt(passphrase));
+				pStmt.execute();
+				DBUtils.closeStmt(pStmt);
 
                 System.out.println("Bastillion for EC2 Generated Global Public Key:");
                 System.out.println(publicKey);
 
-                //set config to default
-                AppConfig.updateProperty("publicKey", "");
-                AppConfig.updateProperty("privateKey", "");
-                AppConfig.updateProperty("defaultSSHPassphrase", "${randomPassphrase}");
+				//set config to default
+				AppConfig.updateProperty("publicKey", "");
+				AppConfig.updateProperty("privateKey", "");
+				AppConfig.updateProperty("defaultSSHPassphrase", "${randomPassphrase}");
 
-                //set to false
-                AppConfig.updateProperty("resetApplicationSSHKey", "false");
+				//set to false
+				AppConfig.updateProperty("resetApplicationSSHKey", "false");
 
-            }
+			}
 
-            //delete ssh keys
-            SSHUtil.deletePvtGenSSHKey();
-            
+			//delete ssh keys
+			SSHUtil.deletePvtGenSSHKey();
 
-        } catch (Exception ex) {
-            log.error(ex.toString(), ex);
-        }
-        finally {
-            DBUtils.closeStmt(statement);
-            DBUtils.closeConn(connection);
-        }
-        
+
+		} catch (Exception ex) {
+			log.error(ex.toString(), ex);
+		}
+		finally {
+			DBUtils.closeStmt(statement);
+			DBUtils.closeConn(connection);
+		}
+
 
     }
 
